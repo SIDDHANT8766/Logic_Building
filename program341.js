a7f18ef28e0033b213caa08fb43f2e005388d553
SIDDHANT8766 <gadkari.siddhant.vivek@gmail.com>
2026-07-11 23:09:21 +0530

    Add files via upload

---
+++ program341.js
@@ -0,0 +1,23 @@
+function CheckEvenOdd(no)
+{
+    if((no % 2) == 0)
+    {
+        console.log("It is Even No");
+    }
+    else
+    {
+        console.log("It is Odd No");
+    }
+}
+
+function main()
+{
+    let iValue = 10;
+
+    CheckEvenOdd(iValue);
+ 
+}
+
+main(); 
+
+
